677affe0b73fd1aa76eb40eb4775a9e9f2d110b7
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-05-22 00:50:22 +0900

    [Bronze V] Title: 새싹, Time: 196 ms, Memory: 17400 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,37 @@
+import java.math.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //* 인풋
+//        int n = sc.nextInt();
+//        int s = sc.nextInt();
+//        int[] nums = new int[n + 1];
+//        for (int i = 0; i < n; i++) {
+//            nums[i] = sc.nextInt();
+//        }
+
+        //* System
+//        int answer = 0;
+//
+//        int sum = nums[0] + nums[1];
+//        int minSum = Integer.MAX_VALUE;
+//
+//        int p2 = 1;
+//        for (int p1 = 0; p1 < n; p1++) {
+//
+//        }
+
+        //* 아웃풋
+//        System.out.println(answer);
+
+        System.out.println("         ,r'\"7\n"
+            + "r`-_   ,'  ,/\n"
+            + " \\. \". L_r'\n"
+            + "   `~\\/\n"
+            + "      |\n"
+            + "      |");
+    }
+}
