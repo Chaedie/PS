17b73846101f8a3bac5f5fc98757da1c78526300
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-05-22 00:55:11 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,24 @@
+import java.math.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //* 인풋
+        int n = sc.nextInt();
+//        int s = sc.nextInt();
+//        int[] nums = new int[n + 1];
+//        for (int i = 0; i < n; i++) {
+//            nums[i] = sc.nextInt();
+//        }
+//        String word = sc.next();
+
+        //* System
+
+        //* 아웃풋
+//        System.out.println(answer);
+        int diff = 2541 - 1998;
+        System.out.println(n - diff);
+    }
+}
