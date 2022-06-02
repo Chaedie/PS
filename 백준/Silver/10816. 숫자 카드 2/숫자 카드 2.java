2e67b521e05d13b05933082b71624a8635603ba9
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-06-03 08:08:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1240 ms, Memory: 192228 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -8,26 +8,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Scanner sc = new Scanner(System.in);
-
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
         int n = Integer.parseInt(br.readLine());
-        int[] numsA = new int[n];
         String[] strA = br.readLine().split(" ");
-        HashMap<Integer,Integer> aMap = new HashMap<>();
+
 
         //* 시스템
         for (int i = 0; i < n; i++) {
-            numsA[i] = Integer.parseInt(strA[i]);
-            aMap.put(numsA[i],aMap.getOrDefault(numsA[i],0) + 1);
+            int key = Integer.parseInt(strA[i]);
+            map.put(key, map.getOrDefault(key,0) + 1);
         }
 
         //* 아웃풋
         int m = Integer.parseInt(br.readLine());
         String[] strB = br.readLine().split(" ");
 
-        int answer = 0;
         for (int i = 0; i < m; i++) {
-            answer = aMap.getOrDefault(Integer.parseInt(strB[i]),0);
-            bw.write(answer + " ");
+            int key = Integer.parseInt(strB[i]);
+            bw.write(map.getOrDefault(key,0) + " ");
         }
         bw.flush();
         br.close();
